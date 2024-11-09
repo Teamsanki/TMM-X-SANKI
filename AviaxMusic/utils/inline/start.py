00736d839cb6e279a8ebed44343094f6dd4c348e
𝐓𝐄𝐀𝐌 𝐒𝐀𝐍𝐊𝐈 <teamsanki001@gmail.com>
2024-11-09 20:28:49 +0530

    Update start.py

--- AviaxMusic/utils/inline/start.py
+++ AviaxMusic/utils/inline/start.py
@@ -31,7 +31,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url=f"https://t.me/ABOUT_VIP_STRANGER"),
+            InlineKeyboardButton(text=_["S_B_7"], url=config.SUPPORT_CHANNEL),
         ],
     ]
     return buttons
